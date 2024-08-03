55b8e50a7f67e99b7c829097caa0e72745fabcba
Amedeka123 <israelamedeka@gmail.com>
2024-08-03 18:52:47 +0000

    update again

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,13 +1,14 @@
 import {test as setup, expect} from "@playwright/test"
 import LoginPage from "../Pages/loginPage"
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();  // Load environment variables from .env file
 const authfile = "playwright/.auth/user.json"
 
 setup("login and store session", async({page})=>{
    const loginPage = new LoginPage(page)
   await page.goto("/")
-  await loginPage.enterUsername("standard_user") //process.env.DEMO_USER!
-  await loginPage.enterPassword("secret_sauce") // process.env.PASSWORD!
+  await loginPage.enterUsername(process.env.DEMO_USER!) //process.env.DEMO_USER!
+  await loginPage.enterPassword(process.env.PASSWORD!) // process.env.PASSWORD!
   await loginPage.clickLoginButton()
   await page.context().storageState({path:authfile})
   await page.close()

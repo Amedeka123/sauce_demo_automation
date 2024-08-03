de9890a6a46a3d687d4a39e381adcc3b0b6396c7
Amedeka123 <israelamedeka@gmail.com>
2024-08-03 18:38:50 +0000

    turn to default yml

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -6,8 +6,8 @@ const authfile = "playwright/.auth/user.json"
 setup("login and store session", async({page})=>{
    const loginPage = new LoginPage(page)
   await page.goto("/")
-  await loginPage.enterUsername(process.env.DEMO_USER!) //process.env.DEMO_USER!
-  await loginPage.enterPassword( process.env.PASSWORD!) // process.env.PASSWORD!
+  await loginPage.enterUsername("standard_user") //process.env.DEMO_USER!
+  await loginPage.enterPassword("secret_sauce") // process.env.PASSWORD!
   await loginPage.clickLoginButton()
   await page.context().storageState({path:authfile})
   await page.close()
